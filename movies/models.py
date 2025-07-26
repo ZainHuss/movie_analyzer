dda970bafff2350c88a4dde593b93fb5acd165f5
zain <zainselman5@gmail.com>
2025-07-26 14:24:45 +0300

    add comment

--- movies/models.py
+++ movies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
-
+#zain
 class Movie(models.Model):
     class Meta:
         verbose_name = _("فيلم")
